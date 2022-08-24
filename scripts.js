cf1a172d23b365b9d685794ea73a6b6723b27c39
Vinicius Felix <38497154+vinicius-felix@users.noreply.github.com>
2022-08-24 13:09:38 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,31 @@
-function calculateTip(event) {
+function calculateTip(event) {	
+	document.getElementById('totaltip').style.display="block";
 	event.preventDefault();
+
 }
 
-let bill = document.getElementById('bill').value;
-let serviceQual = document.getElementById('serviceQual').value;
-let numOfpeople = document.getElementById('people').value;
-let total = 0.00;
 
-if(bill == '' || serviceQual == 0)
-    console.log('Bill cant be null');
+function calcularGorj(){
+	let bill = document.getElementById('bill').value;
+	let serviceQual = document.getElementById('serviceQual').value;
+	let numOfpeople = document.getElementById('people').value;
 
-if(numOfpeople == '' || numOfpeople <= 1){
-    numOfpeople = 1;
-    document.getElementById('each').style.display ="none";
+	if(bill == '' || serviceQual == 0)
+	    console.log('Bill cant be null');
 
-} else 
-    document.getElementById('each').style.display ="block";
+	if(numOfpeople == '' || numOfpeople <= 1){
+	    numOfpeople = 1;
+	    document.getElementById('each').style.display ="none";
 
+	} else 
+	    document.getElementById('each').style.display ="block";
 
-total = (bill  * serviceQual) / numOfpeople;
+	document.getElementById('tip').innerHTML = Number((bill  * serviceQual) / numOfpeople);
+	console.log(bill)
+	console.log(serviceQual)
+	console.log(numOfpeople)
+	document.getElementById('totaltip').style.display="block";
 
-document.getElementById('tip').innerHTML = total.toFixed(2);
-document.getElementById('totaltip').style.display="block";
-
-
-document.getElementById('totaltip').style.display = "none";
-document.getElementById('each').style.display  = "none";
+}
 
 document.getElementById('tipsform').addEventListener('submit', calculateTip);
