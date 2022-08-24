0dfc0ee0c96d5376f37bfa4e1bcc4e3cf65e70cb
Vinicius Felix <38497154+vinicius-felix@users.noreply.github.com>
2022-08-24 09:58:03 -0300

    Fixed some bugs and refactored

--- scripts.js
+++ scripts.js
@@ -6,18 +6,18 @@ let bill = document.getElementById('bill').value;
 let serviceQual = document.getElementById('service Qual').value;
 let numOfpeople = document.getElementById('numOfpeople').value;
 
-if(bill == ''  | serviceQual == 0){
+if(bill == '' || serviceQual == 0)
+    alert('Bill cant be null');
 
-}
-
-if(numOfpeople ==''  numOfpeople <=1){
-    numOfpeople =1;
+if(numOfpeople == '' || numOfpeople <= 1){
+    numOfpeople = 1;
     document.getElementById('each').style.display ="none";
 
-} else{
+} else 
     document.getElementById('each').style.display ="block";
-}
-let total = (bill  * serviceQual) /numoOfpeople;
+
+
+let total = (bill  * serviceQual) / numoOfpeople;
 total = total.tofixed(2);
 document.getElementById('tip').innerHTML = total;
 document.getElementById('totaltip').style.display="block";
